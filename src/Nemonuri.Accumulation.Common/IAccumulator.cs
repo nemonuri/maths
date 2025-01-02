17f959ef14ee29a022394d3c7ea0f9e1a04d83f4
nemonuri <nemonuri2@gmail.com>
2025-01-02 19:43:58 +0900

    add IAccumulator

---
+++ src/Nemonuri.Accumulation.Common/IAccumulator.cs
@@ -0,0 +1,13 @@
+﻿namespace Nemonuri.Accumulation;
+
+public interface IAccumulator<out T, TNumber>
+{
+    T Current { get; }
+
+    bool Accumulate(TNumber number);
+}
+
+public interface IAccumulatable<out T, TNumber>
+{
+    IAccumulator<T, TNumber> GetAccumulator();
+}
