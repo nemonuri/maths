b6322bbb3b939f2e16f7975bb86d7332a15ba724
nemonuri <nemonuri2@gmail.com>
2025-01-13 21:37:53 +0900

    add Nemonuri.Aggregation.Common

---
+++ src/Nemonuri.Aggregation.Common/AggregationTheory.cs
@@ -0,0 +1,52 @@
+﻿namespace Nemonuri.Aggregation;
+
+public static class AggregationTheory
+{
+    public static bool SetIfLess<TItem, TValue>
+    (
+        ref TItem aggregatingItem,
+        ref TValue aggregatingValue,
+        TItem sourceItem,
+        TValue sourceValue
+    )
+        where TValue : IComparable<TValue>
+    {
+        if (aggregatingValue.CompareTo(sourceValue) > 0)
+        {
+            //aggregatingValue < sourceValue
+
+            aggregatingItem = sourceItem;
+            aggregatingValue = sourceValue;
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool SetIfMore<TItem, TValue>
+    (
+        ref TItem aggregatingItem,
+        ref TValue aggregatingValue,
+        TItem sourceItem,
+        TValue sourceValue
+    )
+        where TValue : IComparable<TValue>
+    {
+        if (aggregatingValue.CompareTo(sourceValue) < 0)
+        {
+            //aggregatingValue > sourceValue
+
+            aggregatingItem = sourceItem;
+            aggregatingValue = sourceValue;
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
